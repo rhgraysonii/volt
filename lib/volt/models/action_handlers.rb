f72429000de4fcde999bf6aafbd05d37254f036b
Robert H Grayson II <bobbygrayson@gmail.com>
2015-03-28 14:28:08 -0400

    add base scaffold for action handlers

---
+++ lib/volt/models/action_handlers.rb
@@ -0,0 +1,10 @@
+require 'volt/models/action_handlers/before'
+
+module Volt
+  # include in any class to get action handlers
+  module ActionHandlers
+    module ClassMethods
+    end
+  end
+end
+
---
+++ lib/volt/models/action_handlers/before_action.rb
@@ -0,0 +1,7 @@
+module Volt
+  class BeforeAction
+    def self.before_action(action)
+    end
+  end
+end
+
